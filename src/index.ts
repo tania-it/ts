e07b12860f47ffbb62078cca366c8b60012aee31
none <tania.it-courses@mail.ru>
2023-11-16 21:12:44 +0300

    task 1 in classroom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { greet } from './greet';
+const mainEl = document.getElementById ('main');
+const spanCollection = mainEl 
+? Array.from(mainEl.querySelectorAll('span'))
+:[];
 
-greet ('Hello world!');
+spanCollection.forEach((spanEl) => {
+    const colorValue = spanEl.textContent;
 
-
-
+   if (colorValue) { 
+     spanEl.style.backgroundColor = colorValue
+   }
+});
