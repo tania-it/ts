a7e980286e690bec3fba4a9b54d114440632e90f
none <tania.it-courses@mail.ru>
2023-11-21 20:52:08 +0300

    did event.target task1 in classwork

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { colorSpan } from './colorSpan';
-colorSpan();
+/*import { colorSpan } from './colorSpan';
+colorSpan(); */
 
-/*
+
 const mainEl = document.getElementById('main');
-const spanCollection = mainEl
+/*const spanCollection = mainEl
     ? Array.from(mainEl.querySelectorAll<HTMLElement>('[data-color]'))
     : [];
 
@@ -12,15 +12,18 @@ spanCollection.forEach((spanEl) => {
         const colorValue = spanEl.textContent;
 
         spanEl.style.setProperty('background-color', colorValue);
-        setTimeout(() => {
-            spanEl.style.removeProperty('background-color');
-        }, 1_000);
+        
+    }
+    spanEl.addEventListener('click', paintBg);
+ 
+}); */
+
+mainEl?.addEventListener('click', (event) => {
+    const targetEl = event.target as HTMLElement;
+    const coloredEl = targetEl.closest<HTMLElement>('[data-color]');
+
+    if (coloredEl) {
+        coloredEl.style.setProperty('background-color', coloredEl.textContent);
     }
 
-    spanEl.addEventListener('click', paintBg);
-
-
-    setTimeout(() => {
-        spanEl.removeEventListener('click', paintBg);
-    }, 5_000);
-});*/
+})
