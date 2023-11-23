0af1001579a14229af25b404608d6781090eda02
none <tania.it-courses@mail.ru>
2023-11-23 21:54:23 +0300

    composeValidators & ValidatorFunction

---
+++ src/composeValidators.ts
@@ -0,0 +1,22 @@
+import { ValidatorFunction, nonEmptyArray, requiredText } from "./ValidatorFunction";
+
+export function composeValidators<T>(...validators: ValidatorFunction<T>[]) : ValidatorFunction<T>{
+    return (value: T) : ValidatorResult => {
+        for (const validator of validators) {
+            const result = validator(value);
+
+            if (result) {
+                return result;
+            }
+        }
+        return null;
+    };
+}
+
+
+const requiredFIO =  composeValidators (
+    requiredText,
+    (value) => value.length <= 50 ? null : {maxLength: true},
+);
+
+requiredFIO ('bla');
---
+++ src/form.ts
@@ -0,0 +1,17 @@
+const pizzaOrderForm = document.forms.namedItem('pizzaOrder');
+
+pizzaOrderForm?.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(pizzaOrderForm);
+    const pizzaOrder = {
+        pizzas: formData.getAll ('pizza'),
+        addons: formData.getAll ('addon'),
+        paymentType: formData.get ('paymentType'),
+        customerName: String(formData.get('customerName')).trim(),
+        shippingAddress: String(formData.get('shippingAddress')).trim(),
+    };
+    console.log(pizzaOrder);
+
+});
+
