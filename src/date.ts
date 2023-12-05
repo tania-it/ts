053622086e6031a911d7e12a265a2220fd2399ec
none <tania.it-courses@mail.ru>
2023-12-05 21:53:22 +0300

    did taskDate in class

---
+++ src/date.ts
@@ -0,0 +1,12 @@
+const date = new Date ('December 17, 1995 03:24:00');
+
+const taskItem = document.forms.namedItem('dateForm');
+
+interface taskItem {
+    dateTime: string;
+    text: string;
+
+}
+
+
+
