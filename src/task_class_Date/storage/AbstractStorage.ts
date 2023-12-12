c677744fd74b403b921ff4d854f396223eaf1885
tania-it <tania.it-courses@mail.ru>
2023-12-12 16:59:05 +0300

    fixed AbstactStorage.ts & BrowserLocalStorage.ts

--- src/task_class_Date/storage/AbstractStorage.ts
+++ src/task_class_Date/storage/AbstractStorage.ts
@@ -1,26 +1,26 @@
+
 export abstract class AbstractStorage<Value extends { id: Id }, Id extends number | string, Init = void> {
-// Id тот тип который Promise обещает отдать
-
-readonly #ready: Promise<Init>; // promise - обещание  инициализации
-
-constructor() {
-    this.#ready = this.init();
-}
-get ready(): Promise<Init> {
-    return this.#ready;
-}
-
-init(): Promise<Init> {
-    return Promise.resolve() as Promise<Init>;
-}
-
-abstract get(id: Id): Promise<Value | null>;
-
-abstract getAll(): Promise <Value[]>;
-
-abstract write(data: Omit<Value, 'id'> & { id?: Id}, id?: Id): Promise<Id>;
-
-abstract delete(id: Id): Promise<void>;
-
-
-}
+    readonly #ready: Promise<Init>;
+  
+    constructor() {
+      this.#ready = this.init();
+    }
+  
+    get ready(): Promise<Init> {
+      return this.#ready;
+    }
+  
+    init(): Promise<Init> {
+      return Promise.resolve() as Promise<Init>;
+    }
+  
+    abstract get(id: Id): Promise<Value | null>;
+  
+    abstract getAll(): Promise<Value[]>;
+  
+    abstract write(data: Omit<Value, 'id'> & { id?: Id }, id?: Id): Promise<Id>;
+  
+    abstract delete(id: Id): Promise<void>;
+  
+  }
+  
--- src/task_class_Date/storage/BrowserLocalStorage.ts
+++ src/task_class_Date/storage/BrowserLocalStorage.ts
@@ -1,66 +1,62 @@
-import { AbstractStorage } from "./AbstractStorage";
+import { AbstractStorage } from './AbstractStorage';
 
 export class BrowserLocalStorage<Value extends { id: Id }, Id extends string | number> extends AbstractStorage<Value, Id> {
-    readonly #storageKey: string;
-    readonly #idGenerator: () => Id;
+  readonly #storageKey: string;
+  readonly #idGenerator: () => Id;
 
-    constructor(storageKey: string, idGenerator: () => Id) {
-        super();
-        this.#storageKey = storageKey;
-        this.#idGenerator = idGenerator;
+  constructor(storageKey: string, idGenerator: () => Id) {
+    super();
+    this.#storageKey = storageKey;
+    this.#idGenerator = idGenerator;
+  }
 
-    }
+  async get(id: Id): Promise<Value | null> {
+    await this.ready;
+    const data = this.#extractDataFromStorage();
 
-    get(id: Id): Promise<Value | null> {
-        return this.ready.then(() => {
-            const data = this.#extractDataFromStorage();
-            return data[id] ?? null;
-        });
-    }
+    return data[id] ?? null;
+  }
 
-    async getAll(): Promise<Value[]> {
-        await this.ready;
-        const data = this.#extractDataFromStorage();
-        return Object.values(data);
+  async getAll(): Promise<Value[]> {
+    await this.ready;
+    const data = this.#extractDataFromStorage();
 
-    }
+    return Object.values(data);
+  }
 
   async write(value: Omit<Value, 'id'> & { id?: Id }, id?: Id): Promise<Id> {
-        await this.ready;
-        const data = this.#extractDataFromStorage();
-        const definedId = value.id ?? id ?? this.#idGenerator();
+    await this.ready;
+    const data = this.#extractDataFromStorage();
+    const definedId = value.id ?? id ?? this.#idGenerator();
 
-        this.#saveDataFromStorage({
-            ...data,
-            [definedId]: {
-                ...value,
-                id: definedId,
-            },
-        });
+    this.#saveDataToStorage({
+      ...data,
+      [definedId]: {
+        ...value,
+        id: definedId,
+      },
+    });
 
-        return definedId;
-    }
+    return definedId;
+  }
 
-    async  delete(id: Id): Promise<void> {
-        await this.ready;
-        const data = this.#extractDataFromStorage();
-        delete data[id];
-        this.#saveDataFromStorage(data);
-    }
+  async delete(id: Id): Promise<void> {
+    await this.ready;
+    const data = this.#extractDataFromStorage();
 
-    #extractDataFromStorage(): Record<Id, Value> {
-        const storageData = localStorage.getItem(this.#storageKey);
+    delete data[id];
+    this.#saveDataToStorage(data);
+  }
 
-        return storageData ? JSON.parse(storageData) : {};
+  #extractDataFromStorage(): Record<Id, Value> {
+    const storageData = localStorage.getItem(this.#storageKey);
 
-    }
+    return storageData ? JSON.parse(storageData) : {};
+  }
 
-    #saveDataFromStorage (data: Record<Id, Value>) : void {
+  #saveDataToStorage(data: Record<Id, Value>): void  {
+    const jsonData = JSON.stringify(data);
 
-        const jsonData = JSON.stringify(data);
-        localStorage.setItem(this.#storageKey, jsonData);
-    }
+    localStorage.setItem(this.#storageKey, jsonData);
+  }
 }
-
-
-    
